dfc8c0662c3ebacb960f1da167abb8bd709b9c3e
Sudir Krishnaa RS <sudirkrish17@gmail.com>
2025-01-03 19:21:33 +0530

    Added Nested Objects and it's usage

--- src/18n.js
+++ src/18n.js
@@ -8,20 +8,36 @@ i18n
   .init({
     debug: true,
     fallbackLng: "en",
+    returnObjects: true,
     resources: {
       en: {
         translation: {
           greeting: "Hello, Welcome!",
+          description: {
+            line1:
+              "This POC is made to understand the concepts of Internationalisation of a Web Application with several languages",
+            line2: "Developed by Sudir Krishnaa RS",
+          },
         },
       },
       fr: {
         translation: {
           greeting: "Bonjour, Bienvenue!",
+          description: {
+            line1:
+              "Ce POC est fait pour comprendre les concepts d'Internationalisation d'une Application Web avec plusieurs langues",
+            line2: "Développé par Sudir Krishnaa RS",
+          },
         },
       },
       hi: {
         translation: {
           greeting: "नमस्ते, स्वागत है!",
+          description: {
+            line1:
+              "यह POC कई भाषाओं के साथ एक वेब एप्लिकेशन के अंतर्राष्ट्रीयकरण की अवधारणाओं को समझने के लिए बनाया गया है",
+            line2: "सुदीर कृष्णा आरएस द्वारा विकसित",
+          },
         },
       },
     },
--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import LanguageSelector from "./components/language-selector";
 
 function App() {
   const { t } = useTranslation();
+  const { line1, line2 } = t("description");
 
   return (
     <div className="container">
       <LanguageSelector />
       <h1>{t("greeting")}</h1>
+      <p>{line1}</p>
+      <span>{line2}</span>
     </div>
   );
 }
